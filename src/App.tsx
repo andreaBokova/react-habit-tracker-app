eee6dab7fe7e56853b301e2cf0ab2422f8cbc759
RenaissanceRepo <106615200+andreaBokova@users.noreply.github.com>
2026-07-16 00:41:04 +0200

    added button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { Button } from "./components/Button";
+
 function App() {
-  return <><h1 className="text-red-500">HI</h1></>;
+  return <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
+  <Header></Header>
+  </div>;
 }
 
 export default App;
+
+function Header() {
+  return <header className="flex items-center justify-between">
+    <div className="flex flex-col gap-1">
+    <h1 className="text-3xl font-bold">Habit Tracker</h1>
+    <span className="text-zinc-400 text-sm">1/1 done today</span>
+    </div>
+    <div className="flex flex-col gap-1">
+  <span className="text-zinc-400 text-sm">Apr 6 - Apr 12</span>
+ <div className="flex flex-row gap-3">
+  <Button text="Prev"/>
+  <Button text="Next"/>
+  </div>
+    </div>
+  </header>
+}
