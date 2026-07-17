71297c7ddf7cbe3f22cf6ba00b018cc94eaf5286
RenaissanceRepo <106615200+andreaBokova@users.noreply.github.com>
2026-07-17 17:36:19 +0200

    add habit list - WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Button } from "./components/Button";
 import { Header } from "./components/Header";
 import { HabitForm } from "./components/HabitForm";
 
@@ -13,14 +12,32 @@ export default function App() {
 }
 
 function HabitList() {
+  const habits = [
+    { id: 1, name: "Learn min 1 h", streak: 2 },
+    { id: 2, name: "Gratitude journal", streak: 3 },
+  ];
   return (
-    <div className="">
-    <div className="bg-red-500 flex flex-row gap-4">
-      
-        <span>Practice CSS</span>
-        <span>Streak: 2</span>
-        <button className="flex bg-green-300 flex-1">Delete</button>
-      </div>
+    <div className="flex flex-col gap-80 ">
+      {habits.map((habit) => (
+        <div className="bg-gray-800">
+        <div className="bg-red-500 flex items-center flex-row gap-4">
+          <span key={habit.id}>{habit.name}</span>
+          <span>Streak: {habit.streak}</span>
+          <button className="bg-green-300 ml-auto">Delete</button>
+         
+        </div>
+        <div className="flex justify-evenly gap-6">
+         <div className="rounded-xl bg-pink-200 px-2 flex flex-col text-center"><span>
+          Mon</span><span>6</span></div>
+         <div>Tue</div>
+         <div>Wed</div>
+         <div>Thu</div>
+         <div>Fri</div>
+         <div>Sat</div>
+         <div>Sun</div>
+        </div>
+         </div>
+      ))}
     </div>
   );
 }
